285e9a93b944282a7f2ed65d28b4d92a395136f4
Justen Anderson <justen.anderson@gmail.com>
2022-08-05 14:26:22 -0600

    Add first 3 classes

--- oop.js
+++ oop.js
@@ -3,6 +3,19 @@
 // it should have a color property, that is silver by default
 // it should have a method called "say" that returns whatever string is passed in, with "*~*" at the beginning and end of the string
 
+class Unicorn {
+  constructor(name) {
+    this.name = name;
+    this.color = "silver";
+  }
+
+  say(text) {
+    console.log("*~*" + text + "*~*")
+  }
+}
+
+var jesseTheUnicorn = new Unicorn("Jesse");
+jesseTheUnicorn.say("Hey folks")
 
 
 // Write a class called Vampire
@@ -12,6 +25,23 @@
 // it should have a drink method. When called, the thirsty property changes to false
 
 
+//NOTE - I know this isn't completely correct. I couldn't figure out how to add a default pet that could then be reassigned.
+class Vampire {
+  constructor(name, pet) {
+    this.name = name;
+    this.pet = "bat";
+    this.thirsty = true
+    }
+  changePet(newPet) {
+    this.pet = newPet;
+  }
+  drink() {
+    this.thirsty = false;
+  }
+}
+
+var realPet = new Vampire("Count Chocula","dog")
+console.log(realPet)
 
 //  Write a Dragon class
 // it should have a dynamic name property (string)
@@ -21,7 +51,25 @@
 //  it should have a eat method. If the dragon eats 4 times, it is no longer hungry
   // HINT: This requires you to use your knowledge from the conditionals lesson
 
+  class Dragon {
+  constructor(name, rider, color) {
+    this.name = name;
+    this.rider = rider;
+    this.color = color;
+    this.isHungry = true;
+  };
 
+  eat(meals) {
+    if (meals >= 4) {
+      this.isHungry = false;
+    }
+  };
+}
+
+var myDragon = new Dragon("Rex", "Yous Guys", "black");
+
+myDragon.eat();
+console.log(myDragon);
 
 //  BIG CHALLNEGE: Write a Hobbit class
 //  it should have a dynamic name property (string)
